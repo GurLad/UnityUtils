701b4c8ce07e7d4ce9c3255091fcd678f236ecca
GurLad <40135047+GurLad@users.noreply.github.com>
2020-05-31 19:32:02 +0300

    CrossfadeMusicPlayer fixes

--- Sound/CrossfadeMusicPlayer.cs
+++ Sound/CrossfadeMusicPlayer.cs
@@ -14,10 +14,11 @@ public class CrossfadeMusicPlayer : MonoBehaviour
     public static CrossfadeMusicPlayer Instance;
     public List<CrossfadeMusicPlayerObject> Tracks;
     public float FadeSpeed;
-    [Range(0,1)]
+    [Range(0, 1)]
     public float Volume = 1;
     public bool PlayOnStart;
     public bool KeepTimestamp;
+    public string Playing;
     private AudioSource mainAudioSource;
     private AudioSource seconderyAudioSource;
     private float count;
@@ -42,18 +43,22 @@ public class CrossfadeMusicPlayer : MonoBehaviour
         {
             mainAudioSource.clip = Tracks[0].AudioClip;
             mainAudioSource.Play();
+            Playing = Tracks[0].Name;
         }
     }
     public void Play(string name)
     {
-        if (mainAudioSource.clip == Tracks.Find(a => a.Name == name).AudioClip)
+        CrossfadeMusicPlayerObject target = Tracks.Find(a => a.Name == name);
+        if (target == null)
+        {
+            throw new System.Exception("No matching audio clip! (" + name + ")");
+        }
+        if (mainAudioSource.clip == target.AudioClip)
         {
             return;
         }
-        if ((seconderyAudioSource.clip = Tracks.Find(a => a.Name == name).AudioClip) == null)
-        {
-            throw new System.Exception("No matching audio clip!");
-        }
+        seconderyAudioSource.clip = target.AudioClip;
+        Playing = name;
         mainAudioSource.volume = Volume;
         seconderyAudioSource.volume = 0;
         seconderyAudioSource.Play();
